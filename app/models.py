016d664caa18b3af55a3a16dd5479e98746809dd
ojhaayush9998 <ojhaayush9998@gmail.com>
2025-05-03 14:22:34 +0530

    Initial Commit

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from . import db
+
+class Chemical(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    rfid_tag = db.Column(db.String(120), unique=True, nullable=False)
+
+class MovementLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tag_id = db.Column(db.String(120), nullable=False)
+    location = db.Column(db.String(120), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
---
+++ instance/config.py
@@ -0,0 +1,3 @@
+# Choose SQLite for simplicity
+SQLALCHEMY_DATABASE_URI = 'sqlite:///chemicals.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
